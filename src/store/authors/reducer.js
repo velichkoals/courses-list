672d29f16d733e9bb92071d883a1f884cf2568f3
ALEXEY <velichkoalsprog@gmail.com>
2022-06-02 12:16:25 +0300

    refactor: remove unnecessary comments

--- src/store/authors/reducer.js
+++ src/store/authors/reducer.js
@@ -4,10 +4,6 @@ const defaultState = {
 
 export const authorsReducer = (state = defaultState, action) => {
 	switch (action.type) {
-		// case 'ADD_AUTHOR':
-		// 	return { ...state, authors: state.authors + action.payload };
-		// мб с массивом по другому
-
 		default:
 			return state;
 	}
