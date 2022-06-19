bb2a440aa0718cb96ecd686240a44e3204d906e9
ALEXEY <velichkoalsprog@gmail.com>
2022-06-19 19:38:56 +0300

    feat: add GET requests to courses and authors

--- src/store/authors/reducer.js
+++ src/store/authors/reducer.js
@@ -1,6 +1,19 @@
 import { CREATE_AUTHOR } from './actionTypes';
+import { fetchData } from '../../heplers/fetchData';
 
 const defaultState = [];
+
+export async function getAllAuthors() {
+	const authorsList = await fetchData('GET', '/authors/all');
+	if (authorsList.successful) {
+		authorsList.result.map((author) => {
+			defaultState.push(author);
+			return authorsList;
+		});
+	}
+	return defaultState;
+}
+getAllAuthors().then((r) => r);
 
 export const authorsReducer = (state = defaultState, action) => {
 	switch (action.type) {
--- src/store/courses/reducer.js
+++ src/store/courses/reducer.js
@@ -1,6 +1,19 @@
 import { ADD_COURSE, REMOVE_COURSE } from './actionTypes';
+import { fetchData } from '../../heplers/fetchData';
 
 const defaultState = [];
+
+export async function getAllCourses() {
+	const coursesList = await fetchData('GET', '/courses/all');
+	if (coursesList.successful) {
+		coursesList.result.map((course) => {
+			defaultState.push(course);
+			return coursesList;
+		});
+	}
+	return defaultState;
+}
+getAllCourses().then((r) => r);
 
 export const coursesReducer = (state = defaultState, action) => {
 	switch (action.type) {
